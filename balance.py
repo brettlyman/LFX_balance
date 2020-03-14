90897c74942f53234d66b928785a8545f9f29eeb
Brett Lyman <github@brettlyman.com>
2020-03-13 23:37:16 -0600

    Committing before I add Laurel's new deposit of 15000

--- balance.py
+++ balance.py
@@ -10,12 +10,12 @@ log.setLevel(logging.ERROR)
 
 app = Flask(__name__)
 
-commissions = 0.00
+cirsumvent_commissions = 419.70+570.23
 
 investors = {
 	'lymanfx42': { # even though LymanFX technically isn't an "investor"...
 		'name': 'LymanFX, LLC',
-		'deposit': 6401.00
+		'deposit': 7281.59+cirsumvent_commissions
 	},
 	'ben141': {
 		'name': 'Ben Lyman',
@@ -23,7 +23,7 @@ investors = {
 	},
 	'brett592': {
 		'name': 'Brett Lyman',
-		'deposit': 29000+62582.76+15000
+		'deposit': 68482.30+3030.72+2311.07
 	},
 	'daniel653': {
 		'name': 'Daniel Lyman',
@@ -31,7 +31,7 @@ investors = {
 	},
 	'devin589': {
 		'name': 'Devin Christensen',
-		'deposit': 4000
+		'deposit': 4000+20000
 	},
 	'eddie34': {
 		'name': 'Eddie Lyman',
@@ -51,11 +51,11 @@ investors = {
 	},
 	'mat643': {
 		'name': 'Mathew Lyman',
-		'deposit': 16883.79
+		'deposit': 23992.41
 	},
 	'notben': {
 		'name': 'Ben Bradley',
-		'deposit': 1000
+		'deposit': 1000+3000
 	},
 	'karl': {
 		'name': 'Karl Lilley',
@@ -69,86 +69,17 @@ investors = {
 		'name': 'Abhishek Andhavarapu',
 		'deposit': 2500
 	},
-	'telnicky': {
-		'name': 'Travis Elnicky',
-		'deposit': 1000.00
+	'laurelc': {
+		'name': 'Laurel Christensen',
+		'deposit': 37500.00
 	},
-	'evelyn': {
-		'name': 'Evelyn Sharp',
-		'deposit': 1065.36
+	'toddg': {
+		'name': 'Todd Grant',
+		'deposit': 15000.00
 	}
 }
 
 accounts = [
-	#{
-	#	'account_id': 21962,
-	#	'name': 'Banana Stand - Stable',
-	#	'original_deposit': 36481.31,
-	#	'deposit': 36481.31+stable_commissions,
-	#	'balance': 46114.15,
-	#	'equity':46114.15,
-	#	'reset_balance': 45120.71,
-	#	'period_target': 46345.08,
-	#	'target': 47497.95,
-	#	'lymanfx': 1078.70+stable_commissions,
-	#	'high_water_mark': 41660.50, #40763.01,
-	#	'open_positions': 0,
-	#	'last_update': datetime.now(timezone('US/Mountain')),
-	#	'investors': {
-	#		'ben141': 2597.05,
-	#		'brett592': 12641.04,
-	#		'daniel653': 1493.85,
-	#		'devin589': 2555.74,
-	#		'eddie34': 1676.29,
-	#		'jacob238': 851.91,
-	#		'joe416': 425.96,
-	#		'mat643': 12295.09,
-	#		'notben': 865.68
-	#	}
-	#},
-	#{
-	#	'account_id': 21185,
-	#	'name': 'Banana Stand - Risky',
-	#	'original_deposit': 15346.18,
-	#	'deposit': 15346.18+risky_commissions,
-	#	'balance': 20825.04,
-	#	'equity': 20825.04,
-	#	'reset_balance': 18938.71,
-	#	'period_target': 21084.83,
-	#	'target': 22492.34,
-	#	'lymanfx': 735.72+risky_commissions,
-	#	'high_water_mark': 17890.66,
-	#	'open_positions': 0,
-	#	'last_update': datetime.now(timezone('US/Mountain')),
-	#	'investors': {
-	#		'brett592': 10652.79,
-	#		'mat643': 2387.05,
-	#		'devin589': 1000.00,
-	#		'ben141': 570.62
-	#	}
-	#},
-	#{
-	#	'account_id': 22803,
-	#	'name': 'Bixin',
-	#	'original_deposit': 75000.00,
-	#	'deposit': 75000.00,
-	#	'balance': 83320.69,
-	#	'equity': 83320.69, 
-	#	'reset_balance': 80818.43,
-	#	'period_target': 83495.44,
-	#	'target': 84913.75,
-	#	'open_positions': 0,
-	#	'last_update': datetime.now(timezone('US/Mountain')),
-	#	'investors': {
-	#		'karl': 2500.00,
-	#		'ryan': 2500.00,
-	#		'abhishek': 1643.26,
-	#		'brett592': 64226.02,
-	#		'ben141': 2000.00,
-	#		'evelyn': 1065.36,
-	#		'telnicky': 1065.36
-	#	}
-	#},
 	{
 		'account_id': 496543757,
 		'name': 'Bixin II',
@@ -162,14 +93,40 @@ accounts = [
 		'open_positions': 99,
 		'last_update': datetime.now(timezone('US/Mountain')),
 		'investors': {
-			'brett592': 5252.61,
+			'brett592': 5252.61+1183.55+1183.55,
 			'karl': 2777.36,
 			'ryan': 2777.36,
-			'abhishek': 1825.57,
-			'evelyn': 1183.55,
-			'telnicky': 1183.55
+			'abhishek': 1825.57
 		}
-	}
+	},
+	{
+		'account_id': 496675395,
+		'name': 'Cirsumvent',
+		'original_deposit': 193876.74,
+		'deposit': 193876.74+cirsumvent_commissions,
+		'balance': 196966.26,
+		'equity': 199834.49,
+		'reset_balance': 193876.74,
+		'period_target': 200708.07,
+		'target': 207448.11,
+		'lymanfx': 7281.59+cirsumvent_commissions,
+		'high_water_mark': 188605.62,
+		'open_positions': 958,
+		'last_update': datetime.now(timezone('US/Mountain')),
+		'investors': {
+			'ben141': 6011.81,
+			'brett592': 70000.00,
+			'daniel653': 1732.58,
+			'devin589': 24171.04,
+			'eddie34': 1944.18,
+			'jacob238': 988.06,
+			'joe416': 494.03,
+			'mat643': 24749.43,
+			'notben': 4004.02,
+			'laurelc': 37500.00,
+			'toddg': 15000.00
+		}
+	},
 ]
 
 
@@ -200,13 +157,9 @@ def get_tbody(username):
 		rows.append([investor['name'],None,None,None])
 		rows.append(["Deposit","${:,.2f}".format(investor['deposit']),None,None])
 		investor_total_balance = 0
-		num_accounts = 0
-		total_open_positions = 0
 		for account in accounts:
 			lymanfx_deposit, lymanfx_balance = get_lymanfx_balance(account)
 			if username == 'lymanfx42' or username in account['investors']:
-				num_accounts += 1
-				total_open_positions += account['open_positions']
 				account_name = account['name']
 				
 				rows.append([account_name,None,None,None])
@@ -219,7 +172,7 @@ def get_tbody(username):
 					uncollected_commission = (investors_balance-account['high_water_mark'])/3
 					investors_balance -= uncollected_commission
 				investors_profit = investors_balance-investors_deposit
-				investors_return = round((investors_profit/investors_deposit)*100,1)
+				investors_return = round((investors_profit/investors_deposit)*100,2)
 				if username == "lymanfx42":
 					investor_deposit = lymanfx_deposit
 					investor_balance = lymanfx_balance
@@ -230,14 +183,14 @@ def get_tbody(username):
 				investor_total_balance += investor_balance 
 				if investor_deposit > 0:
 					investor_profit = investor_balance-investor_deposit
-					investor_percent_return = round(((investor_profit)/investor_deposit)*100,1)
+					investor_percent_return = round(((investor_profit)/investor_deposit)*100,2)
 					rows.append(["Deposit/Carryover","${:,.2f}".format(investor_deposit),None,None])
 					rows.append(["Balance","${:,.2f}".format(investor_balance),None,None])
 					rows.append(["Profit/Loss",('-' if investor_profit < 0 else '+')+"${:,.2f}".format(abs(investor_profit)),None,None])
-					rows.append(["Return",('-' if investor_profit < 0 else '+')+"{:,.1f}%".format(abs(investor_percent_return)),None,None])
+					rows.append(["Return",('-' if investor_profit < 0 else '+')+"{:,.2f}%".format(abs(investor_percent_return)),None,None])
 				if username == "lymanfx42":
 					account_profit = account['balance']-account['original_deposit']
-					account_return = round((account_profit/account['original_deposit'])*100,1)
+					account_return = round((account_profit/account['original_deposit'])*100,2)
 					if investor_deposit > 0:
 						rows.append(["---","---","gray",None])
 					rows.append(["Total Deposit","${:,.2f}".format(account['deposit']),None,None])
@@ -248,21 +201,31 @@ def get_tbody(username):
 					rows.append(["Reset Target","${:,.2f}".format(account['target']),"#AAA",None])
 					rows.append(["---","---","gray",None])
 					rows.append(["Profit/Loss",('-' if account_profit < 0 else '+')+"${:,.2f}".format(abs(account_profit)),None,None])
-					rows.append(["Overall Return",('-' if account_return < 0 else '+')+"{:,.1f}%".format(abs(account_return)),None,None])
+					rows.append(["Overall Return",('-' if account_return < 0 else '+')+"{:,.2f}%".format(abs(account_return)),None,None])
 					if 'lymanfx' in account:
 						rows.append(["Investor Balance","${:,.2f}".format(orig_investors_balance),None,None])
 						rows.append(["High Water Mark","${:,.2f}".format(account['high_water_mark']),None,None])
 						rows.append(["Uncollected Commission","${:,.2f}".format(uncollected_commission),"green",None])
 						rows.append(["Adj Investor Balance","${:,.2f}".format(investors_balance),None,None])
 						rows.append(["Investor Profit",('-' if investors_profit < 0 else '+')+"${:,.2f}".format(abs(investors_profit)),None,None])
-						rows.append(["Investor Return",('-' if investors_profit < 0 else '+')+"{:,.1f}%".format(abs(investors_return)),None,None])
+						rows.append(["Investor Return",('-' if investors_profit < 0 else '+')+"{:,.2f}%".format(abs(investors_return)),None,None])
 						rows.append(["---","---","gray",None])
-				rows.append(["Open Units",str(account['open_positions']),None,None])
+					rows.append(["Open Units",str(int(account['open_positions'])),None,None])
+					rows.append(["---","---","gray",None])
+					account_investors = []
+					for inv in account['investors']:
+						if inv != "lymanfx42":
+							inv_deposit = account['investors'][inv]
+							inv_percent = inv_deposit/investors_deposit
+							inv_balance = inv_percent*investors_balance
+							account_investors.append({ 'name': investors[inv]['name'], 'balance': inv_balance })
+					for acct_inv in sorted(account_investors, key=lambda i: i['name']):
+						rows.append([acct_inv['name'],"${:,.2f}".format(acct_inv['balance']),"#AAA",None])
 		investor_total_profit = investor_total_balance-investor['deposit']
-		investor_total_percent_return = round(((investor_total_profit)/investor['deposit'])*100,1)
+		investor_total_percent_return = round(((investor_total_profit)/investor['deposit'])*100,2)
 		rows.insert(2,["Balance","${:,.2f}".format(investor_total_balance),None,None])
 		rows.insert(3,["Profit/Loss",('-' if investor_total_profit < 0 else '+')+"${:,.2f}".format(abs(investor_total_profit)),None,None])
-		rows.insert(4,["Return",('-' if investor_total_profit < 0 else '+')+"{:,.1f}%".format(abs(investor_total_percent_return)),None,None])
+		rows.insert(4,["Return",('-' if investor_total_profit < 0 else '+')+"{:,.2f}%".format(abs(investor_total_percent_return)),None,None])
 	for row in rows:
 		response += template('row_template.html',name=row[0],value=row[1],color=row[2],date_string=row[3])
 	return response
@@ -291,7 +254,7 @@ def balance(username):
 						'$.get("'+tbody_url+'", function (response) { '
 							'$("#main_tbody").html(response);'
 						'}); '
-						'window.setTimeout(update_tbody, 300000); '
+						'window.setTimeout(update_tbody, 30000); '
 					'}'
 				'</script>'
 			'</head>'
@@ -306,7 +269,7 @@ def balance(username):
 				'</table>'
 			'</body>'
 			'<script type="text/javascript">'
-				'window.setTimeout(update_tbody, 300000)'
+				'window.setTimeout(update_tbody, 30000)'
 			'</script>'
 		'</html>')
 
