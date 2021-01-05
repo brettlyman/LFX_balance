35b69c4896f387bd5269d8386405a48a507ac164
Brett Lyman <github@brettlyman.com>
2021-01-04 21:06:28 -0700

    Updates for d'Artagnan

--- balance.py
+++ balance.py
@@ -7,16 +7,14 @@ import os, socket, joblib
 import logging
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
+#logging.basicConfig(filename='error.log',level=logging.DEBUG)
 
 app = Flask(__name__)
 
-cirsumvent_commissions = 0.00 # 419.70+570.23
-cirsumvent_3_commissions = 0.00
-
 investors = {
-	'lymanfx42': { # even though LymanFX technically isn't an "investor"...
+	'lymanfx42': {
 		'name': 'LymanFX, LLC',
-		'deposit': 7281.59+2318.46+cirsumvent_commissions+cirsumvent_3_commissions
+		'deposit': 0.000000001
 	},
 	'ben141': {
 		'name': 'Ben Lyman',
@@ -32,7 +30,7 @@ investors = {
 	},
 	'devin589': {
 		'name': 'Devin Christensen',
-		'deposit': 4000+20000+26000
+		'deposit': 4000+20000+26000+30000
 	},
 	'eddie34': {
 		'name': 'Eddie Lyman',
@@ -58,74 +56,39 @@ investors = {
 		'name': 'Ben Bradley',
 		'deposit': 1000+3000
 	},
-	'karl': {
-		'name': 'Karl Lilley',
-		'deposit': 2500+856.74
-	},
-	'ryan': {
-		'name': 'Ryan Nguyen',
-		'deposit': 2500+856.74
-	},
-	'abhishek': {
-		'name': 'Abhishek Andhavarapu',
-		'deposit': 2500
-	},
 	'laurelc': {
 		'name': 'Laurel Christensen',
-		'deposit': 37500.00+15000.00+30000
+		'deposit': 37500+15000+30000+15000
 	},
 	'toddg': {
 		'name': 'Todd Grant',
-		'deposit': 15000.00
+		'deposit': 15000+15000
 	}
 }
 
 accounts = [
 	{
 		'account_id': 496675395,
-		'name': 'Cirsumvent',
-		'original_deposit': 193876.74,
-		'deposit': 193876.74+cirsumvent_commissions,
-		'balance': 196966.26,
-		'equity': 199834.49,
-		'reset_balance': 193876.74,
-		'period_target': 200708.07,
-		'target': 207448.11,
-		'lymanfx': 7281.59+cirsumvent_commissions,
-		'high_water_mark': 188605.62,
-		'open_positions': 958,
+		'name': 'd\'Artagnan',
+		'original_deposit': 338036.47,
+		'deposit': 338036.47,
+		'balance': 338036.47,
+		'equity': 338036.47,
+		'lymanfx': 0,
+		'high_water_mark': 338036.47,
 		'last_update': datetime.now(timezone('US/Mountain')),
 		'investors': {
-			'ben141': 6011.81,
-			'brett592': 70000.00,
-			'daniel653': 1732.58,
-			'devin589': 24171.04,
-			'eddie34': 1944.18,
-			'jacob238': 988.06,
-			'joe416': 494.03,
-			'mat643': 24749.43,
-			'notben': 4004.02,
-			'laurelc': 37500.00,
-			'toddg': 15000.00
-		}
-	},
-	{
-		'account_id': 497826702,
-		'name': 'Cirsumvent III',
-		'original_deposit': 77955.39,
-		'deposit': 77955.39+cirsumvent_3_commissions,
-		'balance': 77955.39,
-		'equity': 77955.39,
-		'reset_balance': 77955.39,
-		'period_target': 77955.39,
-		'target': 77955.39,
-		'lymanfx': 2318.46,
-		'high_water_mark': 75636.93,
-		'open_positions': 0,
-		'last_update': datetime.now(timezone('US/Mountain')),
-		'investors': {
-			'laurelc': 19636.93+30000,
-			'devin589': 26000
+			'ben141': 6507.55,
+			'brett592': 75772.32,
+			'daniel653': 1875.45,
+			'devin589': 82307.72,
+			'eddie34': 2104.50,
+			'jacob238': 1069.54,
+			'joe416': 534.77,
+			'mat643': 26790.31,
+			'notben': 4334.20,
+			'laurelc': 105503.18,
+			'toddg': 31236.93
 		}
 	}
 ]
@@ -136,10 +99,6 @@ for i in range(0,len(accounts)):
 		account = joblib.load(str(accounts[i]['account_id'])+'.pkl')
 		accounts[i]['balance'] = account['balance']
 		accounts[i]['equity'] = account['equity']
-		accounts[i]['target'] = account['target']
-		accounts[i]['period_target'] = account['period_target']
-		accounts[i]['reset_balance'] = account['reset_balance']
-		accounts[i]['open_positions'] = account['open_positions']
 		accounts[i]['last_update'] = account['last_update']
 
 
@@ -158,8 +117,9 @@ def get_tbody(username):
 		rows.append([investor['name'],None,None,None])
 		rows.append(["Deposit","${:,.2f}".format(investor['deposit']),None,None])
 		investor_total_balance = 0
+		investor_total_equity = 0
 		for account in accounts:
-			lymanfx_deposit, lymanfx_balance = get_lymanfx_balance(account)
+			lymanfx_deposit, lymanfx_balance, lymanfx_equity = get_lymanfx_balance(account)
 			if username == 'lymanfx42' or username in account['investors']:
 				account_name = account['name']
 				
@@ -168,25 +128,33 @@ def get_tbody(username):
 				
 				investors_deposit = account['deposit']-lymanfx_deposit
 				orig_investors_balance = investors_balance = account['balance']-lymanfx_balance
+				investors_equity = account['equity']-lymanfx_equity
 				uncollected_commission = 0
 				if 'high_water_mark' in account and investors_balance > account['high_water_mark']:
 					uncollected_commission = (investors_balance-account['high_water_mark'])/3
 					investors_balance -= uncollected_commission
+				if 'high_water_mark' in account and investors_equity > account['high_water_mark']:
+					uncollected_equity_commission = (investors_equity-account['high_water_mark'])/3
+					investors_equity -= uncollected_equity_commission
 				investors_profit = investors_balance-investors_deposit
 				investors_return = round((investors_profit/investors_deposit)*100,2)
 				if username == "lymanfx42":
 					investor_deposit = lymanfx_deposit
 					investor_balance = lymanfx_balance
+					investor_equity = lymanfx_equity
 				else:
 					investor_deposit = account['investors'][username]
 					investor_percent = investor_deposit/investors_deposit
 					investor_balance = investor_percent*investors_balance
+					investor_equity = investor_percent*investors_equity
 				investor_total_balance += investor_balance 
+				investor_total_equity += investor_equity
 				if investor_deposit > 0:
 					investor_profit = investor_balance-investor_deposit
 					investor_percent_return = round(((investor_profit)/investor_deposit)*100,2)
 					rows.append(["Deposit/Carryover","${:,.2f}".format(investor_deposit),None,None])
 					rows.append(["Balance","${:,.2f}".format(investor_balance),None,None])
+					rows.append(["Live Balance","${:,.2f}".format(investor_equity),"#AAA",None])
 					rows.append(["Profit/Loss",('-' if investor_profit < 0 else '+')+"${:,.2f}".format(abs(investor_profit)),None,None])
 					rows.append(["Return",('-' if investor_profit < 0 else '+')+"{:,.2f}%".format(abs(investor_percent_return)),None,None])
 				if username == "lymanfx42":
@@ -195,11 +163,8 @@ def get_tbody(username):
 					if investor_deposit > 0:
 						rows.append(["---","---","gray",None])
 					rows.append(["Total Deposit","${:,.2f}".format(account['deposit']),None,None])
-					rows.append(["Reset Balance","${:,.2f}".format(account['reset_balance']),"#AAA",None])
 					rows.append(["Closed Balance","${:,.2f}".format(account['balance']),None,None])
 					rows.append(["Open Balance","${:,.2f}".format(account['equity']),"green" if account['equity'] > account['balance'] else None,None])
-					rows.append(["Period Target","${:,.2f}".format(account['period_target']),"#AAA",None])
-					rows.append(["Reset Target","${:,.2f}".format(account['target']),"#AAA",None])
 					rows.append(["---","---","gray",None])
 					rows.append(["Profit/Loss",('-' if account_profit < 0 else '+')+"${:,.2f}".format(abs(account_profit)),None,None])
 					rows.append(["Overall Return",('-' if account_return < 0 else '+')+"{:,.2f}%".format(abs(account_return)),None,None])
@@ -210,8 +175,6 @@ def get_tbody(username):
 						rows.append(["Adj Investor Balance","${:,.2f}".format(investors_balance),None,None])
 						rows.append(["Investor Profit",('-' if investors_profit < 0 else '+')+"${:,.2f}".format(abs(investors_profit)),None,None])
 						rows.append(["Investor Return",('-' if investors_profit < 0 else '+')+"{:,.2f}%".format(abs(investors_return)),None,None])
-						rows.append(["---","---","gray",None])
-					rows.append(["Open Units",str(int(account['open_positions'])),None,None])
 					rows.append(["---","---","gray",None])
 					account_investors = []
 					for inv in account['investors']:
@@ -225,8 +188,9 @@ def get_tbody(username):
 		investor_total_profit = investor_total_balance-investor['deposit']
 		investor_total_percent_return = round(((investor_total_profit)/investor['deposit'])*100,2)
 		rows.insert(2,["Balance","${:,.2f}".format(investor_total_balance),None,None])
-		rows.insert(3,["Profit/Loss",('-' if investor_total_profit < 0 else '+')+"${:,.2f}".format(abs(investor_total_profit)),None,None])
-		rows.insert(4,["Return",('-' if investor_total_profit < 0 else '+')+"{:,.2f}%".format(abs(investor_total_percent_return)),None,None])
+		rows.insert(3,["Live Balance","${:,.2f}".format(investor_total_equity),"#AAA",None])
+		rows.insert(4,["Profit/Loss",('-' if investor_total_profit < 0 else '+')+"${:,.2f}".format(abs(investor_total_profit)),None,None])
+		rows.insert(5,["Return",('-' if investor_total_profit < 0 else '+')+"{:,.2f}%".format(abs(investor_total_percent_return)),None,None])
 	for row in rows:
 		response += template('row_template.html',name=row[0],value=row[1],color=row[2],date_string=row[3])
 	return response
@@ -235,7 +199,7 @@ def get_tbody(username):
 def get_lymanfx_balance(account):
 	if 'lymanfx' in account:
 		lymanfx_percent = account['lymanfx']/account['deposit']
-		return account['lymanfx'], lymanfx_percent * account['balance']
+		return account['lymanfx'], lymanfx_percent * account['balance'], lymanfx_percent * account['equity']
 	return 0, 0
 
 
@@ -260,6 +224,7 @@ def balance(username):
 				'</script>'
 			'</head>'
 			'<body style="font-family: Arial;">'
+				'<!-- <h1>UNDER CONSTRUCTION</h1><p>(moving money around, updating accounting, etc.)</p> -->'
 				'<table style="width: 300px">'
 					'<thead>'
 						'<tr>'
@@ -285,10 +250,6 @@ def update():
 			if accounts[i]['account_id'] == account_id:
 				accounts[i]['balance'] = float(request.args.get('b',''))
 				accounts[i]['equity'] = float(request.args.get('e',''))
-				accounts[i]['target'] = float(request.args.get('t',''))
-				accounts[i]['period_target'] = float(request.args.get('pt',''))
-				accounts[i]['reset_balance'] = float(request.args.get('rb',''))
-				accounts[i]['open_positions'] = float(request.args.get('n',''))
 				accounts[i]['last_update'] = datetime.now(timezone('US/Mountain'))
 				joblib.dump(accounts[i], str(accounts[i]['account_id'])+'.pkl')
 				return 'successful'
